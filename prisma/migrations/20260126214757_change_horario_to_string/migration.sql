34dd79198c94359472c87d64fabb2272da015724
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2026-01-27 01:44:23 -0300

    Convert horario column to VARCHAR

--- prisma/migrations/20260126214757_change_horario_to_string/migration.sql
+++ prisma/migrations/20260126214757_change_horario_to_string/migration.sql
@@ -2,8 +2,10 @@
   Warnings:
 
   - Changed the type of `horario` on the `clientes` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
 ALTER TABLE "clientes" DROP COLUMN "horario",
 ADD COLUMN     "horario" VARCHAR(8) NOT NULL;
+*/
+-- AlterTable
+
+-- AlterTable
+ALTER TABLE "clientes" ALTER COLUMN "horario" TYPE VARCHAR(8) USING TO_CHAR("horario", 'HH24:MI:SS');
