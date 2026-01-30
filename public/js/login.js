5ce5a87e4319505bbac44de1870b89bfebdaa385
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2026-01-30 02:30:32 -0300

    correcao Render localhost para api render,

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,10 @@
  * ✅ Exibe mensagens de erro vindo do header
  */
 
-const API_URL = "http://localhost:3001/api";
+// No login.js e em outros arquivos JS de componente
+const API_URL = window.location.origin.includes('localhost') 
+  ? "http://localhost:3001/api" 
+  : "/api"; // Em produção, ele usará a mesma URL do site
 let emailRecuperacaoGlobal = null;
 let tokenRecuperacaoGlobal = null;
 
